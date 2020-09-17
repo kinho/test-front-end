08665f01720dfb57f45cb9262d3b70394efb9e70
Marcus Vinicius Sgorla Franklin <kiinho@gmail.com>
2020-09-17 00:04:13 -0300

    feat: implemented debounced filter on user list

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { AutoSizer, Column, Table } from 'react-virtualized'
-// import styled from 'styled-components'
-
-import { Form, Col } from 'react-bootstrap'
+import { Form, Col, Spinner } from 'react-bootstrap'
+import _ from 'lodash'
 
 import { getUsers } from '../../services/user'
 
@@ -10,64 +9,81 @@ import 'react-virtualized/styles.css'
 import './styles.css'
 
 export default () => {
+  const [loading, setLoading] = useState(true)
   const [users, setUsers] = useState([])
+  const [filteredUsers, setFilteredUsers] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     const load = async () => {
       const resp = await getUsers()
 
       if (resp.status !== 200) {
-        alert('error')
+        return
       }
 
       const { data: { data } } = resp
 
-      console.log('users', data)
       setUsers(data)
+      setFilteredUsers(data)
+      setLoading(false)
     }
 
     load()
   }, [])
 
+  const changeSearch = ({ target: { value } }) => {
+    setSearch(value)
+    debouncedSearch(value)
+  }
+
+  const debouncedSearch = _.debounce(s => filterList(s), 800)
+
+  const filterList = (value) => {
+    const arrFiltered = users.filter(({ name, age }) => (
+      !!name.includes(value) || !!`${age}`.includes(value)
+    ))
+    setFilteredUsers(arrFiltered)
+  }
+
+  const WrappedSpinner = () => (
+    <div className="d-flex justify-content-center">
+      <Spinner animation="border" />
+    </div>
+  )
+
   return (
-    <div class="d-flex justify-content-center">
-      <div class="wrapper-p">
+    <div className="d-flex justify-content-center">
+      <div className="wrapper-p">
         <Form>
           <Form.Row>
             <Col>
-              <Form.Control placeholder="Search by Name" />
-            </Col>
-            <Col>
-              <Form.Control placeholder="Search by Age" />
+              <Form.Control 
+                placeholder="Search"
+                onChange={changeSearch}
+                value={search}
+                disabled={!!loading}
+              />
             </Col>
           </Form.Row>
         </Form>
 
         <div className="wrapper-list">
           <AutoSizer>
-            {
-              ({ width, height }) => (
-                <Table
-                  width={width}
-                  height={height}
-                  headerHeight={50}
-                  rowHeight={30}
-                  rowCount={users.length}
-                  rowGetter={({ index }) => users[index]}
-                >
-                  <Column
-                    label='Name'
-                    dataKey='name'
-                    width={300}
-                  />
-                  <Column
-                    label='Age'
-                    dataKey='age'
-                    width={100}
-                  />
-                </Table>
-              )
-            }
+            {({ width, height }) => (
+              <Table
+                width={width}
+                height={height}
+                headerHeight={50}
+                rowHeight={30}
+                rowCount={filteredUsers.length}
+                rowGetter={({ index }) => filteredUsers[index]}
+                noRowsRenderer={() => loading ? <WrappedSpinner /> : null}
+              >
+                <Column label='Name' dataKey='name' width={300} />
+                <Column label='Age' dataKey='age' width={200} />
+              </Table>
+            )}
           </AutoSizer>
         </div>
       </div>
